556d58e1b0738c45563148320623daaef6e040ba
lord-of-cinder-2000 <vuvanquyet17102@gmail.com>
2023-03-10 07:55:32 +0700

    add version 1.0.6: x-count header

---
+++ IGDB/HttpClients/HttpHeadersExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Net.Http.Headers;
+
+namespace IGDB.HttpClients
+{
+    public static class HttpHeadersExtensions
+    {
+        public static long GetXCountHeader(this HttpHeaders headers)
+        {
+            long count = 0;
+            if (headers.TryGetValues("X-Count", out var values))
+            {
+                var value = values.FirstOrDefault();
+                if (value != null)
+                {
+                    if (long.TryParse(value, out var countValue))
+                    {
+                        count = countValue;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ IGDB/HttpClients/XCountHeaderHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IGDB.HttpClients
+{
+    public class XCountHeaderHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+
+            long totalCount = response.Headers.GetXCountHeader();
+
+            response.Content.Headers.Add("X-Count", totalCount.ToString());
+            return response;
+        }
+    }
+}
